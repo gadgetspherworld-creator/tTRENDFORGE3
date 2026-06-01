1cfa9302b8fafb0a93b513ad71ecff7464ab0375
gadgetspherworld-creator <gadgetspherworld@gmail.com>
2026-06-01 16:23:38 +0200

    Delete apps directory

--- apps/web/app/(dashboard)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Sidebar } from '../../components/ui/sidebar'
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex min-h-screen">
-      <Sidebar />
-      <main className="flex-1 ml-60 p-8 overflow-auto">{children}</main>
-    </div>
-  )
-}
